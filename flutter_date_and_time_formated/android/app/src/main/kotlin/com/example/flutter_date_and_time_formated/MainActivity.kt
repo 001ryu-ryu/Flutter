96a7ed609cdd5e324e60cdd02d72b015858ef723
Iftikar Hussain <iftikarraza8@gmail.com>
2024-11-14 14:25:13 +0530

    Learnt some DateTime, DateTime picker and GridView, GridView Build

---
+++ flutter_date_and_time_formated/android/app/src/main/kotlin/com/example/flutter_date_and_time_formated/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_date_and_time_formated
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_gridview/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  var color1 = Colors.purple;
+  var color2 = Colors.blue;
+  var color3 = Colors.deepOrange;
+  var color4 = Colors.green;
+  var color5 = Colors.grey;
+}
