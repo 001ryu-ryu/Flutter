976dc78024e147fcfffacdd8f89c9aaddb0cf611
Iftikar Hussain <iftikarraza8@gmail.com>
2024-11-17 13:04:24 +0530

    learnt ListWheel Scrolling

---
+++ flutter_listwheel_scrollview/README.md
@@ -0,0 +1,7 @@
+# flutter_listwheel_scrollview
+
+A new Flutter project.
+
+## Getting Started
+
+3D List
---
+++ flutter_listwheel_scrollview/android/app/src/main/kotlin/com/example/flutter_listwheel_scrollview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_listwheel_scrollview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
